20759a8119a5428722549ef0e01cf323cba78236
eliaslamanna <eliaslamanna@hotmail.com>
2025-06-26 21:57:46 -0300

    app levantando

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb+srv://fmessina:Test2025DESA@cluster0.rfzx4fd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('✅ Conectado a MongoDB');
+  })
+  .catch((err) => {
+    console.error('❌ Error al conectar MongoDB:', err);
+  });
 
 app.use(cors());
 app.use(express.json());
--- app/api/model/Usuario.ts
+++ app/api/model/Usuario.ts
@@ -1,3 +1,4 @@
+import mongoose, { Document, Schema } from 'mongoose';
 import api from '../api';
 import { handleApiError } from '../errorHandler';
 
@@ -7,6 +8,16 @@ export interface Usuario {
   alias: string;
   nombre: string;
 }
+
+export interface IUsuarioDocument extends Document, Omit<Usuario, 'id'> {}
+
+const UsuarioSchema = new Schema<IUsuarioDocument>({
+  email: { type: String, required: true },
+  alias: { type: String, required: true },
+  nombre: { type: String, required: true }
+});
+
+module.exports = mongoose.model('users', UsuarioSchema);
 
 export const getSavedRecipes = async (userId: string) => {
   try {
