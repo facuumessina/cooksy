626e2c541fd9c715c1092f3b9290811e12c3046f
eliaslamanna <eliaslamanna@hotmail.com>
2025-07-03 03:08:00 -0300

    render deploy

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,38 +2,31 @@
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-
 import authRoutes from './routes/authRoutes';
 import recipeRoutes from './routes/recipeRoutes';
 import userRoutes from './routes/userRoutes';
 
-import './model/Receta';
-import './model/Usuario';
-
 const app = express();
-const port = process.env.PORT ?? 3000;
 
-// Conexión a MongoDB
+// Render inyecta el puerto por env
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
 mongoose
-  .connect('mongodb+srv://fmessina:Test2025DESA@cluster0.rfzx4fd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .connect(process.env.MONGODB_URI!)
   .then(() => console.log('✅ Conectado a MongoDB'))
   .catch(err => console.error('❌ Error al conectar MongoDB:', err));
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas
-app.use('/auth',    authRoutes);
+app.use('/auth', authRoutes);
 app.use('/recipes', recipeRoutes);
-app.use('/users',   userRoutes);
+app.use('/users', userRoutes);
 
-// Endpoint de prueba
 app.get('/test', (_req, res) => {
   res.send('funcionó');
 });
 
-// Arrancar servidor
 app.listen(port, () => {
   console.log(`API corriendo en http://localhost:${port}`);
 });
